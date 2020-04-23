4e6e5a42aeb76bafa835ec88fc49c45c257c207b
Ubuntu <ubuntu@ip-172-31-35-111.ec2.internal>
2020-04-23 09:58:50 +0000

    last commit

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendEmail (contactForm) {
+    emailjs.send ("gmail" , "rosie",{
+        "from_name":contactForm.name.value,
+        "from_email" : contactForm.emailaddress.value,
+        "project_request": contactForm.projectSummary.value
+    })
+    
+    .then(
+        function(response){
+            console.log("SUCCESS" , response);
+      },
+      
+      function(error) {
+          console.log("FAILED", error);
+      });
+}
